42e7f5491032259a3574416c7c67580dd1ac417e
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-19 18:36:59 +0600

    Update 3_Input_from_user.py

--- 1_Starting_with_Python/3_Input_from_user.py
+++ 1_Starting_with_Python/3_Input_from_user.py
@@ -6,3 +6,12 @@ print("Hello,", name)
 age = int(input("Enter Your Age: "))    # Convert to int upon taking input
 
 print("Hello," + name + ".You are", age,"years old.")   # In case of string, we can concatenate using + instead of comma
+
+x,y = input("Enter two Numbers: ").split()
+x = int(x)       # Convert first input
+y = int(y)       # Convert second input
+print(f"Sum = {x + y}")
+
+# This is random example that you can do it too
+
+print("Hi joha, Your id is: ",input("Enter your id: "))    # input() will be executed first in this line
