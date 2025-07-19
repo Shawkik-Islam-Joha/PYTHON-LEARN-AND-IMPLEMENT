cbc69c6ceea260a8d61d79ae0b205dd68493c36e
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-20 00:39:11 +0600

    Add temperature conversion module and example script

---
+++ 4_Modules/1_Importing_Modules.py
@@ -0,0 +1,9 @@
+import tempConv
+
+x = int(input("Enter the Temperature in Celcius : "))
+
+print("Temperature in Kelvin :",tempConv.cel_to_kelvin(x),"K")
+
+# The above code imports the tempConv module, takes user input for temperature in Celsius,
+# and prints the temperature converted to Kelvin using the cel_to_kelvin function.
+
---
+++ 4_Modules/tempConv.py
@@ -0,0 +1,5 @@
+# 1_Temperature_Convert.py
+
+def cel_to_kelvin(temperature):
+    return temperature+273
+
