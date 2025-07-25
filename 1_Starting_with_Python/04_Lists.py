90e189a201804deb4cf5a5d535759d206a0051b8
SHAWKIK ISLAM JOHA <shawkikislam@gmail.com>
2025-07-25 18:47:16 +0600

    Updated Comments in 04_Lists.py

--- 1_Starting_with_Python/04_Lists.py
+++ 1_Starting_with_Python/04_Lists.py
@@ -32,6 +32,8 @@ numbers.pop(2)           # Removes 9
 
 print(numbers)
 
+# new_list = [(expression) for (item) in (iterable) if (condition)]
+
 squares = [x**2 for x in range(1, 6)]   # [1, 4, 9, 16, 25]
 
 print(squares)
